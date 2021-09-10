6f2cc1af7bfc20a46e29de515a06f688107dc361
akakss225 <akakss225@naver.com>
2021-09-10 13:32:35 +0900

    0910_class3

---
+++ JAVA/Class0910/src/Class3.java
@@ -0,0 +1,19 @@
+
+public class Class3 {
+	
+	public static int multi(int n, int m) {
+		return n * m;
+	}
+
+	public static void main(String[] args) {
+		// static : 미리 준비된다는 의미. >> 실행 메모리에 올라가서 실행될 수 있도록 준비됨.
+		//     		인스턴스를 생성하지 않고 실행을 시킬 때 static을 해준다.
+		
+		// 위의 함수의 경우 main에서 호출을 해야하기 때문에 static을 붙혀준다.
+		
+		
+		System.out.println(123);
+
+	}
+
+}
---
+++ JAVA/Class0910/src/class1.java
@@ -0,0 +1,29 @@
+
+public class class1 {
+
+	public static void main(String[] args) {
+		
+		/** 
+		* IDE( Integrated Development Environment ) : 통합 개발 환경
+		* -편집기 + 컴파일러 + 디버거 등이 통합되어 제공됨.
+		* 
+		* Java SE :자바 표준 배포판 ( Standard Edition )
+		* Java ME : 자바 마이크로 배포판. 휴대전화나 PDA, 셋톱박스등 제한된 리소스를갖는 하드웨어
+		* Java EE : 자바 기업용 배포판. 
+		* 
+		* 자바 API : Application Programming Interface. JDK에 포함된 클래스 라이브러리.
+		* 자바 패키지 : 서로 관련된 클래스들을 분류하여 묶어놓은 것. 계층구조로 되어있음.
+		* 
+		*
+		*
+		*
+		*/
+		
+		
+		
+		
+		
+
+	}
+
+}
