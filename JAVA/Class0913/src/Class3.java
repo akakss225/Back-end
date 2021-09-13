3c4a60e28da007b983856c3cefdd4120f95e20fd
akakss225 <akakss225@naver.com>
2021-09-13 13:35:37 +0900

    class3

--- JAVA/Class0913/src/Class3.java
+++ JAVA/Class0913/src/Class3.java
@@ -2,8 +2,17 @@
 public class Class3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// 삼항연산.
+		int w = 3, z = 5;
+		
+		System.out.println("두 수의 차는 : " + ((w>z)? (w-z):(z-w)));
+		
+		byte a = 5;
+		
+		byte b = (byte)(a<<2);
+		
+		System.out.println(a + ", " + b);
 	}
 
 }
