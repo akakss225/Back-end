35bff1d3d3f68793bd6d8ad8912ef81e7db6f86f
akakss225 <akakss225@naver.com>
2021-09-14 18:03:00 +0900

    practice6

---
+++ JAVA/Class0914/src/Practice6.java
@@ -0,0 +1,25 @@
+
+public class Practice6 {
+
+	public static void main(String[] args) {
+//		5. 다음과 같은 형식으로 출력하시오.이중for문
+//	     *
+//	     ***
+//	    *****
+//	   *******  
+//	  *********
+		
+		for(int i = 0; i < 10; i++) {
+			for(int j = 1; j < 10 -i; j++) {
+				System.out.print(" ");
+				
+			}
+			for(int k = 0; k < 2 * i + 1; k++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
