9173ab5e84fee6f15afe06c76033f3982d1bbfc5
akakss225 <akakss225@naver.com>
2021-09-14 13:32:48 +0900

    class2

---
+++ JAVA/Class0914/src/Class2.java
@@ -0,0 +1,23 @@
+
+public class Class2 {
+
+	public static void main(String[] args) {
+
+		// DO ~ While
+		
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i < 10);
+		
+		char c = 'a';
+		
+		do {
+			System.out.print(c + " ");
+			c = (char)(c+1);
+		}while(c <= 'z');
+
+	}
+
+}
