c2874ebd40e561193363581f98191d7da0a07832
akakss225 <akakss225@naver.com>
2021-09-14 15:56:40 +0900

    practice1

---
+++ JAVA/Class0914/src/practice1.java
@@ -0,0 +1,16 @@
+
+public class practice1 {
+
+	public static void main(String[] args) {
+
+//		0. 구구단을 종적으로 2단부터 9단까지 출력하기.
+		
+		for(int i = 2; i < 10; i ++) {
+			for (int j = 2; j < 10; j++) {
+				System.out.println(" " + i +  " * " + j + " = " +i*j );
+			}
+			System.out.println();
+		}
+	}
+
+}
