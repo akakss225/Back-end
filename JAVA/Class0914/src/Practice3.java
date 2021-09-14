23635e550981749f4264baece974ac9b10271ba9
akakss225 <akakss225@naver.com>
2021-09-14 16:24:21 +0900

    practice3

---
+++ JAVA/Class0914/src/Practice3.java
@@ -0,0 +1,16 @@
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+
+//		2. 1부터 100까지 3의 배수만 출력하기. continue사용
+		
+		for(int i = 1; i < 101; i++) {
+			if(i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
