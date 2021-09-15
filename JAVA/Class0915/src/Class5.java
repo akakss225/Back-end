753401bdab924d3108181e280fd8f671f6fc900b
akakss225 <akakss225@naver.com>
2021-09-15 11:08:13 +0900

    class5

--- JAVA/Class0915/src/Class5.java
+++ JAVA/Class0915/src/Class5.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class Class5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수 3개를 입력해주세요.");
+		int sum = 0, n = 0;
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.print(i + " >> ");
+			n = sc.nextInt();
+			try {
+				System.out.println(n);
+				sum += n;
+			}
+			catch(Exception e) {
+				System.out.println("정수가 아닙니다. 다시입력하세요.");
+				sc.nextInt();
+				i--;
+				continue;
+			}
+		}
+		System.out.print("입력된 정수의 합은 " + sum + " 입니다.");
 
 	}
 
