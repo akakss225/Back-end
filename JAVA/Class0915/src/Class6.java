5567d03098fe5b3309cb693771e83bb4b87d9f9a
akakss225 <akakss225@naver.com>
2021-09-15 11:37:50 +0900

    class6

---
+++ JAVA/Class0915/src/Class6.java
@@ -0,0 +1,23 @@
+
+public class Class6 {
+
+	public static void main(String[] args) {
+
+		String[] numbers = {"23", "12", "3.141592", "998"};
+		
+		int i = 0;
+		
+		try {
+			for(i = 0; i < numbers.length; i++) {
+				int j = Integer.parseInt(numbers[i]);
+				System.out.println("숫자로 변환된 값 : " + j);
+			}
+		}
+		catch(NumberFormatException e) {
+			System.out.println(numbers[i] + " 는 정수로 변환할 수 없습니다.");
+			
+		}
+
+	}
+
+}
