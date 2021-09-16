a17848e346462e01bbe727e1e3c44cc86af3c1a6
akakss225 <akakss225@naver.com>
2021-09-16 14:19:47 +0900

    class13

---
+++ JAVA/Class0916/src/Inheritance4/Class13.java
@@ -0,0 +1,13 @@
+package Inheritance4;
+
+public class Class13 {
+
+	public static void main(String[] args) {
+		
+		ColorPoint cp = new ColorPoint(5, 6, "blue");
+		
+		cp.showColorPoint();
+
+	}
+
+}
