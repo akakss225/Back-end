67b531a87d01dac50e0097a12bb1dd2bb152df5f
akakss225 <akakss225@naver.com>
2021-09-17 10:18:15 +0900

    class1

--- JAVA/Class0917/src/Class1.java
+++ JAVA/Class0917/src/Class1.java
@@ -1,5 +1,7 @@
 
 class Shape{
+	public Shape next;
+	public Shape() {next = null;}
 	public void draw() {
 		System.out.println("Shape");
 	}
@@ -40,6 +42,34 @@ public class Class1 {
 		paint(new Rect());
 		paint(new Circle());
 		
+		
+		System.out.println("----------------------------------");
+		
+		Shape start, last, obj;
+		
+		start = new Line();
+		last = start;
+		obj = new Line();
+		last.next = obj;
+		last = obj;
+		
+		obj = new Rect();
+		last.next = obj;
+		last = obj;
+		
+		obj = new Circle();
+		last.next = obj;
+		last = obj;
+		
+		obj = new Shape();
+		last.next = obj;
+		last = obj;
+		
+		Shape p = start;
+		while(p != null) {
+			p.draw();
+			p = p.next;
+		}
 
 	}
 
