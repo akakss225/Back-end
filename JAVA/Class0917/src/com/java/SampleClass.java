50051a8be61f0fe54b926a963ed4557a2bace8c8
akakss225 <akakss225@naver.com>
2021-09-17 14:13:31 +0900

    class8

--- JAVA/Class0917/src/com/java/SampleClass.java
+++ JAVA/Class0917/src/com/java/SampleClass.java
@@ -1,9 +1,11 @@
 package com.java;
 
+import com.java.graphic.GraphicTest;
+
 public class SampleClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GraphicTest gtc = new GraphicTest();
 
 	}
 
--- JAVA/Class0917/src/com/java/graphic/GraphicTest.java
+++ JAVA/Class0917/src/com/java/graphic/GraphicTest.java
@@ -1,5 +1,5 @@
 package com.java.graphic;
 
 public class GraphicTest {
-
+	
 }
