7fd7f05990dab3c17683dce392824a83ff32b1c5
akakss225 <akakss225@naver.com>
2021-09-17 11:19:20 +0900

    class5

---
+++ JAVA/Class0917/src/AbstractPractice/Class5.java
@@ -0,0 +1,38 @@
+package AbstractPractice;
+
+abstract class Calculator{
+	public abstract int add(int a, int b);
+	public abstract int subtract(int a, int b);
+	public abstract double average(int[] a);
+}
+
+class GoodCalc extends Calculator{
+	@Override
+	public int add(int a, int b) {
+		return a + b;
+	}
+	@Override
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	@Override
+	public double average(int[] a) {
+		double sum = 0;
+		for (int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		return sum/a.length;
+	}
+}
+public class Class5 {
+
+	public static void main(String[] args) {
+		
+		GoodCalc c = new GoodCalc();
+		
+		System.out.println(c.add(5, 10));
+		System.out.println(c.subtract(11, 2));
+		System.out.println(c.average(new int[] {1,2,3,4,5}));
+	}
+
+}
