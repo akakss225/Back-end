b4fbaba53c1c460b4d59550297dbdf36ed6683d0
akakss225 <akakss225@naver.com>
2021-09-23 18:06:25 +0900

    숫자야구게임 만들기

--- JAVA/Class0917/src/practice/BaseballGame.java
+++ JAVA/Class0917/src/practice/BaseballGame.java
@@ -8,7 +8,7 @@ class BaseballGame {
 	// 컴퓨터 랜덤숫자 만들 그릇.
 	public ArrayList<Integer> arr = new ArrayList<Integer>();
 	// 내가 입력해서 컴퓨터 숫자와 비교할 숫자 그릇.
-	ArrayList<String> answer = new ArrayList<String>();
+	ArrayList<Integer> answer = new ArrayList<Integer>();
 	// 맞췄을때 리턴할 값.
 	String end = "homerun";
 	int tried = 0;
@@ -43,64 +43,70 @@ class BaseballGame {
 		// 숫자 입력
 		Scanner scan = new Scanner(System.in);
 		String now = "initial";
+		int strikeCount = 0;
+		int ballCount = 0;
+		System.out.println(arr);
 		
 		while(!now.equals(end)) {
 			System.out.print("Please Write a numbers(3) : ");
 			String check = scan.nextLine();
 			StringTokenizer st = new StringTokenizer(check, " ");
 			while(st.hasMoreTokens()) {
-				answer.add(st.nextToken());
+				answer.add(Integer.parseInt(st.nextToken()));
 			}
 			
-			int strikeCount = 0;
-			int ballCount = 0;
 			for(int i = 0; i < answer.size(); i++) {
-				if(answer.get(i).equals(arr.get(i))) {
+				if(this.answer.get(i) == this.arr.get(i)) {
 					strikeCount += 1;
 				}
-				else if(arr.contains(answer.get(i))) {
+				else if(this.arr.contains(this.answer.get(i))) {
 					ballCount += 1;
 				}
 			}
+			
 			if(ballCount == 0 && strikeCount == 0) {
 				tried += 1;
 				now = "OUT!";
 				System.out.println(now);
-				answer.removeAll(answer);
+				answer.clear();
 				
 			}
 			else if(ballCount != 0 || strikeCount != 3) {
 				tried += 1;
 				now = strikeCount +  " strike " + ballCount + " ball ~ ";
 				System.out.println(now);
-				answer.removeAll(answer);
+				answer.clear();
+				ballCount = 0;
+				strikeCount = 0;
 			}
 			else if(strikeCount == 3) {
 				tried += 1;
 				now = end;
 				System.out.println(now);
-				answer.removeAll(answer);
+				answer.clear();
+				ballCount = 0;
+				strikeCount = 0;
 			}
 			
 		}
 		System.out.println("How many times You needed : " + tried);
 		switch(tried) {
-		case 1: System.out.println("WTF! You are Lucky guy!");
+		case 1: System.out.println("WTF! You are Lucky guy!"); break;
 		case 2: System.out.println("WTF! You are Lucky guy!"); break;
-		case 3: System.out.println("ohh~~ You are a Genius"); 
-		case 4: System.out.println("ohh~~ You are a Genius"); 
+		case 3: System.out.println("ohh~~ You are a Genius"); break;
+		case 4: System.out.println("ohh~~ You are a Genius"); break;
 		case 5: System.out.println("ohh~~ You are a Genius"); break;
-		case 6: System.out.println("You are a Good player!");
-		case 7: System.out.println("You are a Good player!");
-		case 8: System.out.println("You are a Good player!");
-		case 9: System.out.println("You are a Good player!");
+		case 6: System.out.println("You are a Good player!"); break;
+		case 7: System.out.println("You are a Good player!"); break;
+		case 8: System.out.println("You are a Good player!"); break;
+		case 9: System.out.println("You are a Good player!"); break;
 		case 10: System.out.println("You are a Good player!"); break;
-		case 11: System.out.println("Nice play");
-		case 12: System.out.println("Nice play");
+		case 11: System.out.println("Nice play"); break;
+		case 12: System.out.println("Nice play"); break;
 		case 13: System.out.println("Nice play"); break;
-		case 14: System.out.println("You need more effort");
-		case 15: System.out.println("You need more effort");
-		case 16: System.out.println("You need more effort");
+		case 14: System.out.println("You need more effort"); break;
+		case 15: System.out.println("You need more effort"); break;
+		case 16: System.out.println("You need more effort"); break;
 		case 17: System.out.println("You need more effort"); break;
 		default : System.out.println("Bad plat kkkkkkk!!"); break;
 		}
--- JAVA/Class0917/src/practice/Practice1.java
+++ JAVA/Class0917/src/practice/Practice1.java
@@ -6,6 +6,7 @@ public class Practice1 {
 
 	public static void main(String[] args) {
 		Play p = new Play();
+		
 	}
 
 }
