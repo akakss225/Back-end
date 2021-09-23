66b62c729afe94573242167c171544b4f5357cec
akakss225 <akakss225@naver.com>
2021-09-23 15:41:23 +0900

    practice3

---
+++ JAVA/Class0923/src/practice/Practice3.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+		// 3. 상품코드(E20160001)에서 상품카테고리('E')를 추출하시오.
+		
+		String code = "E20160001";
+		
+		System.out.println(code.charAt(0));
+
+	}
+
+}
