89901e989fb2004cdf2c784deec2b4880523a9f4
akakss225 <akakss225@naver.com>
2021-09-23 15:42:53 +0900

    practice4

---
+++ JAVA/Class0923/src/practice/Practice4.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Practice4 {
+
+	public static void main(String[] args) {
+		// 4. 상품코드(E20160001)에서 년도('2016')를 추출하시오.
+		
+		String code = "E20160001";
+		
+		System.out.println(code.substring(1, 5));
+
+	}
+
+}
