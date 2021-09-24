2094683bbd03efc10688e950fa014a2da24457f6
akakss225 <akakss225@naver.com>
2021-09-24 11:46:00 +0900

    class6

---
+++ JAVA/Class0924/src/Class6.java
@@ -0,0 +1,23 @@
+import java.io.FileReader;
+
+public class Class6 {
+
+	public static void main(String[] args) {
+		
+		FileReader fin = null;
+		
+		try {
+			fin = new FileReader("test");
+			int c;
+			while((c = fin.read()) != -1) {
+				System.out.print((char)c);
+			}
+			fin.close();
+		}
+		catch(IOExeption e) {
+			System.out.println("입출력 오류");
+		}
+
+	}
+
+}
