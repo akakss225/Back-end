152abf83de1ac4009f58e832447812caa225d343
akakss225 <akakss225@naver.com>
2021-09-24 12:20:21 +0900

    class8

---
+++ JAVA/Class0924/src/Class8.java
@@ -0,0 +1,32 @@
+import java.io.BufferedOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Class8 {
+
+	public static void main(String[] args) {
+		
+		FileReader fin = null;
+		int c;
+		try {
+			fin = new FileReader("/Users/sumin/Desktop/Back-end/JAVA/Class0924/src/test.txt");
+			BufferedOutputStream out = new BufferedOutputStream(System.out, 5);
+			while((c = fin.read()) != -1) {
+				out.write(c);
+			}
+			new Scanner(System.in).nextLine();
+			out.flush();
+			fin.close();
+			out.close();
+			
+		}
+		catch(IOException e) {
+			
+		}
+		
+		
+		
+	}
+
+}
