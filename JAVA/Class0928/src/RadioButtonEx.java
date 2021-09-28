b29671d0145f803923621fb58e5eac5ecf21dcd0
akakss225 <akakss225@naver.com>
2021-09-28 15:53:11 +0900

    RadioButtonEx

--- JAVA/Class0928/src/RadioButtonEx.java
+++ JAVA/Class0928/src/RadioButtonEx.java
@@ -1,9 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class RadioButtonEx {
+public class RadioButtonEx extends JFrame {
+	
+	public RadioButtonEx() {
+		this.setTitle("라디오버튼 만들기 예제");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		ImageIcon cherryIcon = new ImageIcon("/Users/sumin/Desktop/Back-end/명품JAVA프로그래밍(개정4판)_학습자용[20210811]/예제 및 그림 소스/11장/chap11-ex06/images/cherry.jpg");
+		ImageIcon selectedCherryIcon = new ImageIcon("/Users/sumin/Desktop/Back-end/명품JAVA프로그래밍(개정4판)_학습자용[20210811]/예제 및 그림 소스/11장/chap11-ex06/images/selectedCherry.jpg");
+		
+		ButtonGroup g = new ButtonGroup();
+		JRadioButton apple = new JRadioButton("사과");
+		JRadioButton pear = new JRadioButton("배", true);
+		JRadioButton cherry = new JRadioButton("체리", cherryIcon);
+		cherry.setBorderPainted(true);
+		cherry.setSelectedIcon(selectedCherryIcon);
+		
+		g.add(apple);
+		g.add(pear);
+		g.add(cherry);
+		
+		c.add(apple);
+		c.add(pear);
+		c.add(cherry);
+		
+		this.setSize(250, 150);
+		this.setVisible(true);
+		
+		
+		
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ddd
+		new RadioButtonEx();
 
 	}
 
