f11646e6585d1a1a84787f6454a7b254900fe434
akakss225 <akakss225@naver.com>
2021-09-29 10:50:49 +0900

    GraphicsFillEx

--- JAVA/Class0929/src/Grammar.java
+++ JAVA/Class0929/src/Grammar.java
@@ -9,10 +9,20 @@ public class Grammar {
 //	Font : 폰트를 표현하는 클래스
 //	생성자 >> Font(String fontFace, int style, int size) 글꼴, 스타일(고딕, 이텔릭 등과같은), 크기
 	
-	도형 그리기
-	1. drawLine : 시작점과 끝점을 설정
+//	도형 그리기
+//	1. drawLine : 시작점과 끝점을 설정
+//	2. drawOval(int x, int y, int width, int height) : 원 그리기 
+//	3. drawRect(int x, int y, int width, int height) : 사각형 그리기
+//	4. drawRoundRect(int x, int y, int width, int height, int roundX, int roundY) : 모서리가 둥근 사각형 그리기
+//	5. drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) : 원 호 >> start angle은 원호의 시작 각도. arcAngle은 그리는 각도
+//	6. drawPolygon(int[] x, int[] y, int n) : 다각형 그리기 int[] x 는 x좌표만 저장됨, int[] y는 y좌표만 저장됨. n은 꼭지점의 갯수이다.
 	
-	
+//	도형 칠하기
+//	1. fillOval(int x, int y, int width, int height)
+//	2. fillRect(int x, int y, int width, int height)
+//	3. fillRoundRect(int x, int y, int width, int height, int roundX, int roundY)
+//	4. fillArc(int x, int y, int width, int height, int startAngle, int arcAngle))
+//	5. fillPolygon(int[] x, int[] y, int n)
 	
 	
 }
---
+++ JAVA/Class0929/src/GraphicsDrawImageEx1.java
@@ -0,0 +1,9 @@
+
+public class GraphicsDrawImageEx1 {
+
+	public static void main(String[] args) {
+		new GraphicsDrawImageEx1();
+
+	}
+
+}
