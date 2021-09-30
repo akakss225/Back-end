87008b32d07cae846c8a9cac61f0f04591466d7b
akakss225 <akakss225@naver.com>
2021-09-30 16:02:06 +0900

    RunnableTimerEx

--- JAVA/Class0930/src/RunnableTimerEx.java
+++ JAVA/Class0930/src/RunnableTimerEx.java
@@ -1,3 +1,8 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 class TimerRunnable implements Runnable{
@@ -10,16 +15,46 @@ class TimerRunnable implements Runnable{
 	@Override
 	public void run() {
 		int n = 0;
+		while(true) {
+			timerLabel.setText(Integer.toString(n));
+			n++;
+			try {
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e) {
+				return;
+			}
+		}
 		
 	}
 	
 }
 
-public class RunnableTimerEx {
+public class RunnableTimerEx extends JFrame {
+	public RunnableTimerEx() {
+		this.setTitle("Thread Timer by Runnable");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JLabel timerLabel = new JLabel();
+		timerLabel.setFont(new Font("Gothic", Font.ITALIC, 80));
+		c.add(timerLabel);
+		
+		TimerRunnable runnable = new TimerRunnable(timerLabel);
+		Thread th = new Thread(runnable);
+		
+		this.setSize(250, 150);
+		this.setVisible(true);
+		
+		th.start();
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-dd
+		
+		new RunnableTimerEx();
 	}
 
 }
