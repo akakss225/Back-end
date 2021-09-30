7f7b34d9219da27bb8ae8b93dd72c8ed9371f931
akakss225 <akakss225@naver.com>
2021-09-30 16:24:12 +0900

    ThreadInterruptEx

--- JAVA/Class0930/src/ThreadInterruptEx.java
+++ JAVA/Class0930/src/ThreadInterruptEx.java
@@ -1,9 +1,76 @@
-ddd
-public class ThreadInterruptEx {
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+class TimerRunnable2 implements Runnable{
+	private JLabel timerLabel;
+	
+	public TimerRunnable2(JLabel timerLabel) {
+		this.timerLabel = timerLabel;
+	}
+
+	@Override
+	public void run() {
+		int n = 0;
+		while(true) {
+			timerLabel.setText(Integer.toString(n));
+			n++;
+			try {
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e) {
+				return;
+			}
+		}
+		
+	}
+	
+}
+
+public class ThreadInterruptEx extends JFrame {
+	private Thread th;
+	public ThreadInterruptEx() {
+		this.setTitle("ThreadInterruptEx");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		JLabel timerLabel = new JLabel();
+		timerLabel.setFont(new Font("Gothic", Font.ITALIC, 80));
+		
+		TimerRunnable2 runnable = new TimerRunnable2(timerLabel);
+		Thread th = new Thread(runnable);
+		c.add(timerLabel);
+		
+		JButton btn = new JButton("Kill Timer");
+		btn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				th.interrupt();
+				JButton btn = (JButton)e.getSource();
+				btn.setEnabled(false); // 버튼 비활성화
+			}
+			
+		});
+		c.add(btn);
+		
+		this.setSize(300, 170);
+		this.setVisible(true);
+		
+		th.start();
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new ThreadInterruptEx();
 	}
 
 }
