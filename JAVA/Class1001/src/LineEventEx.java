9fbc84ef1320889747a75a6f5a3d4d9c72ccd507
akakss225 <akakss225@naver.com>
2021-10-01 16:53:39 +0900

    LineEventEx

--- JAVA/Class1001/src/LineEventEx.java
+++ JAVA/Class1001/src/LineEventEx.java
@@ -1,13 +1,69 @@
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.io.File;
 
-14 - 13
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 public class LineEventEx extends JFrame {
+	
+	private Clip clip;
+	private String song = "/Users/sumin/Desktop/Back-end/명품JAVA프로그래밍(개정4판)_학습자용[20210811]/예제 및 그림 소스/14장/chap14-ex13/audio/애국가1절.wav";
+	private JLabel label = new JLabel(song);
+	
+	public LineEventEx() {
+		this.setTitle("애국가 1절 연주");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		
+		c.setLayout(new FlowLayout());
+		c.setBackground(Color.YELLOW);
+		c.add(label);
+		this.setSize(300,150);
+		this.setVisible(true);
+		
+		loadAudio(song);
+		
+	}
+	private void loadAudio(String pathName) {
+		try {
+			File audioFile = new File(pathName);
+			final AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+			clip = AudioSystem.getClip();
+			
+			clip.addLineListener(new LineListener() {
+				public void update(LineEvent e) {
+					if(e.getType() == LineEvent.Type.STOP) {
+						try {
+							getContentPane().setBackground(Color.ORANGE);
+							label.setText(song + "연주 끝");
+							audioStream.close();
+						}
+						catch(Exception e1) {
+							e1.printStackTrace();
+						}
+					}
+				}
+			});
+			clip.open(audioStream);
+			clip.start();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new LineEventEx();
 	}
 
 }
