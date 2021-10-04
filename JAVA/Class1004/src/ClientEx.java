41aebca1cc1e3f3e3b73eea277da33228706eb05
akakss225 <akakss225@naver.com>
2021-10-04 10:46:19 +0900

    ClientEx

--- JAVA/Class1004/src/ClientEx.java
+++ JAVA/Class1004/src/ClientEx.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -10,17 +11,35 @@ public class ClientEx {
 	public static void main(String[] args) {
 		BufferedReader in = null;
 		BufferedWriter out = null;
-		ServerSocket listener = null;
 		Socket socket = null;
 		Scanner sc = new Scanner(System.in);
 		try {
-			
+			socket = new Socket("localHost", 9999);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			while(true) {
+				System.out.println("보내기 >>");
+				String outputmsg = sc.nextLine();
+				if(outputmsg.equalsIgnoreCase("bye")) {
+					out.write(outputmsg + "\n");
+					out.flush();
+					break;
+				}
+				out.write(outputmsg + "\n");
+				out.flush();
+				String inputmsg = in.readLine();
+				System.out.println("서버 : " + inputmsg);
+				
+			}
 		}
 		catch(IOException e) {
-			
+			e.printStackTrace();
 		}
 		finally {
 			try {
+				if(sc != null) {
+					sc.close();
+				}
 				if(socket != null) { // null point exception, 이게 좋은코드임
 					socket.close();
 				}
