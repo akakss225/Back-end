dfa52123c18933578d3d8a98d5206d831e7db1be
akakss225 <akakss225@naver.com>
2021-10-21 15:40:49 +0900

    Num1

---
+++ JAVA/Test1/src/Num1.java
@@ -0,0 +1,15 @@
+
+public class Num1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
