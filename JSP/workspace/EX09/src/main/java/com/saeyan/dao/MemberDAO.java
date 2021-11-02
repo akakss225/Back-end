97be28445b9d27ff8e2f5ada7337f6917b021d54
akakss225 <akakss225@naver.com>
2021-11-02 11:38:57 +0900

    DBCP 설치 및 간단한 예제test

---
+++ JSP/workspace/EX09/src/main/java/com/saeyan/dao/MemberDAO.java
@@ -0,0 +1,25 @@
+package com.saeyan.dao;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	// 싱글톤 패턴
+	private MemberDAO() {} // 생성자는 private
+	
+	private static MemberDAO instance = new MemberDAO(); // private static instance 직접 생성
+	
+	public static MemberDAO getInstance() { // instance getter 생성
+		return instance;
+	}
+	public Connection getConnection() throws Exception{
+		Connection conn = null;
+		Context init = new InitialContext();
+		DataSource ds = (DataSource)init.lookup("java:comp/env/jdbc/TestDB");
+		conn = ds.getConnection();
+		return conn;
+	}
+}
---
+++ JSP/workspace/EX09/src/main/webapp/Member.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<sql:query var="rs" dataSource="jdbc/TestDB">
+	select * from member
+</sql:query>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<c:forEach var="row" items="${rs.rows}">
+	    Name ${row.name}<br/>
+	    ID ${row.userid}<br/>
+</c:forEach>
+</body>
+</html>
