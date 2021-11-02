ee10b61d36a8368700fedb4d93902b613277ba46
akakss225 <akakss225@naver.com>
2021-11-02 15:11:59 +0900

    EX09_DB를 이용한 회원관리 프로그램_ 로그인 확인 및 회원정보확인 form

---
+++ JSP/workspace/EX09/src/main/webapp/script/member.js
@@ -0,0 +1,12 @@
+function loginCheck(){
+	if(document.frm.userid.value.length==0){
+		alert("id를 입력하세요.");
+		frm.userid.focus();
+		return false;
+	}
+	if(document.frm.pwd.value==""){
+		alert("비밀변호를 입력하세요.");
+		frm.pwd.focus();
+		return false;
+	}
+}
